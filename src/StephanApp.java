a37fbefe0290cf4000357347aa3651281226f4f8
Hack Soomin <diane.rha@buzzvil.com>
2019-05-18 22:55:31 +0900

    커밋합니다

---
+++ src/StephanApp.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class StephanApp {
+	public static void main(String[] args) {
+		StephanApp stephanApp = new StephanApp();
+		stephanApp.start();
+
+	}
+
+	private void start() {
+		startMainMenu();
+	}
+
+	private void startMainMenu() {
+		while (true) {
+			System.out.println("MainMenu");
+			System.out.println("1. addNote");
+			System.out.println("2. listNote");
+			System.out.println("3. finish");
+
+			System.out.println("type menu num: ");
+			Scanner scanner = new Scanner(System.in);
+			int num = scanner.nextInt();
+
+			if (num == 1) {
+				startAddNoteScreen();
+			} else if (num == 2) {
+				startListNoteScreen();
+			} else if (num == 3) {
+				finish();
+				break;
+			} else {
+				System.out.println("miss typing");
+			}
+		}
+	}
+
+	private void finish() {
+		System.out.println("finish app");
+	}
+
+	private void startListNoteScreen() {
+		System.out.println( "Note1");
+		System.out.println( "Note2");
+		System.out.println( "Select your note");
+
+
+	}
+
+	private void startAddNoteScreen() {
+
+	}
+
+}
